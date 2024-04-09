f867dab22feefd54876500f8a5261984bd4d93e1
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2024-04-09 09:00:36 -0400

    update theme, add expansion panels to resource manager

--- src/app/resource-manager/resource-manager.component.ts
+++ src/app/resource-manager/resource-manager.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'resource-manager',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule, MatIconModule],
   templateUrl: './resource-manager.component.html',
   styleUrl: './resource-manager.component.scss'
 })
